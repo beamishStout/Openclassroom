a7184459b9f40afcc35468e7420a3e7251c89402
beamishStout <louis.aneiros@gmail.com>
2017-01-30 00:47:52 +0100

    ajouté fonction en itératif

--- Fonctions_de_passage_de_bases.py
+++ Fonctions_de_passage_de_bases.py
@@ -19,3 +19,23 @@ def de10en2Rec(n):
         else :
             rep = aux(n)
         return rep
+
+## Passage d'un nombre base 10 en bas 2 (Itératif)
+def de10en2Iter(n):
+    if (n>=0):
+        rep=""
+        if (n==0):
+            rep="0"
+        #while (n>0):
+        while (n!=0):
+            ## version plus lisible
+            reste=n%2
+            n=n/2
+            if (reste==1):
+                rep="1"+rep
+            else:
+                rep="0"+rep
+            ## version condensee
+            ## rep = str(n%2) + rep
+            ## n=n/2
+        return rep
